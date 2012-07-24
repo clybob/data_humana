8d898e117afcfb4aa0dde8ba6c10cfd44d013822
Romulo Costa Tavares <clybob@hotmail.com>
2012-07-24 14:12:52 -0300

    Criado pacote para pip

---
+++ README.txt
@@ -0,0 +1,20 @@
+Data_humana 0.1.0
+
+Essa lib tem por objetivo retornar em texto a diferença entre um data qualquer,
+passada por parâmetro e a data atual.
+
+- Pluralizado: Quantidades já estão no plural
+- Suporta as seguintes medidas:
+* X anos atrás
+* 1 ano atrás
+* X meses atrás
+* 1 mes atrás
+* X semanas atrás
+* 1 semana atrás
+* X dias atrás
+* 1 dia atrás
+* X horas atrás
+* 1 hora atrás
+* X minutos atrás
+* 1 minuto atrás
+* menos de um minuto atrás
--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 # encoding: utf-8
 
 from distutils.core import setup
-from setuptools import find_packages
 
 setup(
     name='data_humana',
     version='0.1.0',
     author='Romulo Tavares',
     author_email='clybob@hotmail.com',
-    packages=find_packages(),
+    packages=['data_humana'],
     license='GPL',
-    url='git://github.com/clybob/data_humana.git',
-    description='Dada uma determinada data, calcula a diferença entre ela ' +
-                'e a data atual e apresenta em forma de texto, ex: 1 ano atrás'
+    url='https://github.com/clybob/data_humana.git',
+    description='Dada uma determinada data, calcula a diferença entre ela e ' +
+                'a data atual e apresenta em forma de texto, ex: 1 ano atrás',
+    long_description=open('README.txt').read(),
+    install_requires=[]
 )
